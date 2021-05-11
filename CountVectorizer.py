18014c6bc9e15b6856bde8e063565aadc71c7ffb
Jager <jagerli@icloud.com>
2021-05-11 18:34:52 +1200

    MultinomialNB class complete, errr...... almost!

--- CountVectorizer.py
+++ CountVectorizer.py
@@ -1,17 +1,17 @@
 #  Copyright (c) 2021 Jäger. All rights reserved.
-import collections
 import numpy as np
+from typing import List
 
 
 class CountVectorizer:
 
-    def __init__(self, stop_words, max_features=None):
+    def __init__(self, stop_words: List[str], max_features: int = None):
         self.vocabularies = []
         self.stop_words = stop_words
         self.max_features = max_features
         pass
 
-    def fit(self, raw_documents: np.ndarray, y=None):
+    def fit(self, raw_documents) -> None:
         raw_documents_all_in_1 = ' '.join(raw_documents)
         all_words = np.array(self.text_process(raw_documents_all_in_1), dtype=object)
         unique, counts = np.unique(all_words, return_counts=True)
@@ -29,6 +29,6 @@ class CountVectorizer:
                 vectorized_document[i, j] = words.count(self.vocabularies[:, 0][j])
         return vectorized_document
 
-    def text_process(self, abstract: str):
+    def text_process(self, abstract: str) -> List[str]:
         return [word.lower() for word in ''.join([char for char in abstract if char != '"']).split() if
                 word not in self.stop_words]
--- MultinomialNB.py
+++ MultinomialNB.py
@@ -1,11 +1,32 @@
 #  Copyright (c) 2021 Jäger. All rights reserved.
 import numpy as np
-class MultinomialNB:
-    def __init__(self)->None:
-        pass
-    def fit(self, X_train, y_train):
-        class_unique = np.array([np.unique(y_train), np.arange(len(np.unique(y_)))])
-        p_Y_alone = np.zeros(len(class_unique))
-        for i in range(p_Y_alone):
-            p_Y_alone
 
+
+class MultinomialNB:
+    def __init__(self) -> None:
+        self.y_labels = []
+        self.grouped_Xs = []
+        self.class_priors = []
+        self.likelihoods_by_class = []
+        pass
+
+    def fit(self, vec_X, y):
+        self.y_labels = np.unique(y).tolist()
+        self.grouped_Xs = [vec_X[y == self.y_labels[i]] for i in range(len(self.y_labels))]
+        self.class_priors = [vec_x.shape[0] / vec_X.shape[0] for vec_x in self.grouped_Xs]
+        self.likelihoods_by_class = [(vec_x.sum(axis=0) + 1) / (vec_x.sum() + len(vec_X.shape[1])) for vec_x in
+                                     self.grouped_Xs]
+
+    def predict(self, vectorized_samples):
+        predictions = []
+        for sample in vectorized_samples:
+            temp = []  # list of 4 posterior_pr
+            for c in range(len(self.grouped_Xs)):
+                sample_likelihood = 1.0
+                for i in range(len(sample)):  # for word in words
+                    for j in range(sample[i]):
+                        sample_likelihood = sample_likelihood * self.likelihoods_by_class[c][i]
+                temp.append(sample_likelihood * self.class_priors[c])
+            print(temp)
+            predictions.append(self.y_labels[temp.index(np.max(temp))])
+        return predictions
