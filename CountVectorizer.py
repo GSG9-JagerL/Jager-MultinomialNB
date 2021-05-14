ce4a4cdb17bdc5ff497ac8240182f33334daed67
Jager <jagerli@icloud.com>
2021-05-14 15:51:30 +1200

    All done, for this stage!

--- CountVectorizer.py
+++ CountVectorizer.py
@@ -1,4 +1,6 @@
 #  Copyright (c) 2021 Jäger. All rights reserved.
+import string
+
 import numpy as np
 from typing import List
 
@@ -16,19 +18,32 @@ class CountVectorizer:
         all_words = np.array(self.text_process(raw_documents_all_in_1), dtype=object)
         unique, counts = np.unique(all_words, return_counts=True)
         bag_of_words = np.transpose(np.array([unique, counts], dtype=object))
-        self.vocabularies = bag_of_words[bag_of_words[:, 1].argsort()]
-        self.vocabularies = self.vocabularies[::-1]
+        self.vocabularies = bag_of_words[bag_of_words[:, 1].argsort()]  # sort
+        self.vocabularies = self.vocabularies[::-1]  # descending order
         if self.max_features is not None:
             self.vocabularies = self.vocabularies[:self.max_features]
 
-    def transform(self, raw_documents):
+    def transform(self, raw_documents, tf_idf=False):
         vectorized_document = np.empty(shape=(len(raw_documents), len(self.vocabularies)), dtype=object)
         for i in range(len(raw_documents)):
             words = self.text_process(raw_documents[i])
             for j in range(len(self.vocabularies)):
                 vectorized_document[i, j] = words.count(self.vocabularies[:, 0][j])
-        return vectorized_document
+        if tf_idf:
+            return vectorized_document, CountVectorizer.idf(vectorized_document)
+        else:
+            return vectorized_document
 
     def text_process(self, abstract: str) -> List[str]:
-        return [word.lower() for word in ''.join([char for char in abstract if char != '"']).split() if
+        return [word.lower() for word in ''.join([char for char in abstract if char not in string.punctuation]).split()
+                if
                 word not in self.stop_words]
+
+    # noinspection PyPep8Naming
+    @staticmethod
+    def idf(vectorized_document: np.ndarray) -> np.ndarray:
+        N = len(vectorized_document)
+        D = [len(vectorized_document[vectorized_document[:, j] != 0]) for j in range(vectorized_document.shape[1])]
+        D = np.array(D, dtype=np.float)
+        IDF = np.log(N+1 / D+1)+1
+        return IDF
--- MultinomialNB.py
+++ MultinomialNB.py
@@ -1,5 +1,7 @@
 #  Copyright (c) 2021 Jäger. All rights reserved.
+
 import numpy as np
+from math import log, pow, exp
 
 
 class MultinomialNB:
@@ -10,12 +12,16 @@ class MultinomialNB:
         self.likelihoods_by_class = []
         pass
 
-    def fit(self, vec_X, y):
+    def fit(self, vec_X, y, idf=None):
         self.y_labels = np.unique(y).tolist()
         self.grouped_Xs = [vec_X[y == self.y_labels[i]] for i in range(len(self.y_labels))]
         self.class_priors = [vec_x.shape[0] / vec_X.shape[0] for vec_x in self.grouped_Xs]
-        self.likelihoods_by_class = [(vec_x.sum(axis=0) + 1) / (vec_x.sum() + len(vec_X.shape[1])) for vec_x in
-                                     self.grouped_Xs]
+        if idf is None:
+            self.likelihoods_by_class = [(vec_x.sum(axis=0) + 1) / (vec_x.sum() + vec_X.shape[1]) for vec_x in
+                                         self.grouped_Xs]
+        else:
+            self.likelihoods_by_class = [(vec_x.sum(axis=0) * idf + 1) / (sum(vec_x.sum(axis=0) * idf) + vec_X.shape[1])
+                                         for vec_x in self.grouped_Xs]
 
     def predict(self, vectorized_samples):
         predictions = []
@@ -23,10 +29,44 @@ class MultinomialNB:
             temp = []  # list of 4 posterior_pr
             for c in range(len(self.grouped_Xs)):
                 sample_likelihood = 1.0
+                # log_sample_likelihood = log(sample_likelihood)
                 for i in range(len(sample)):  # for word in words
                     for j in range(sample[i]):
+                        # log_sample_likelihood = log_sample_likelihood + log(self.likelihoods_by_class[c][i])
                         sample_likelihood = sample_likelihood * self.likelihoods_by_class[c][i]
-                temp.append(sample_likelihood * self.class_priors[c])
-            print(temp)
+                # temp.append(exp(sample_likelihood + log(self.class_priors[c])))
+                temp.append(sample_likelihood*self.class_priors[c])
+            # print(temp)
             predictions.append(self.y_labels[temp.index(np.max(temp))])
         return predictions
+
+def get_accuracy(estimates, true_val, verbose = False):
+    n = 0
+    for pair in np.array([estimates, true_val]).transpose():
+        if pair[0] == pair[1]:
+            n += 1
+    if verbose:
+        print(f"Correct: {n}\tAccuracy: {n / len(estimates):.4%}")
+    return n / len(estimates)
+
+
+def cross_val_score(X: np.ndarray, y: np.ndarray, idf=None, cv: int = 5):
+    index_list = np.arange(X.shape[0])
+    indices_folds = np.asarray(np.array_split(index_list, cv))
+    results = np.zeros(cv)
+    for i in range(cv):
+        val_fold_indices = indices_folds[i]
+        # np.delete(indices_folds, i)
+        train_fold_indices = indices_folds[[j for j in range(cv) if j!=i]].flatten()
+        X_val = X[val_fold_indices]
+        y_val = y[val_fold_indices]
+        X_train = X[train_fold_indices]
+        y_train = y[train_fold_indices]
+        model = MultinomialNB()
+        if idf is not None:
+            model.fit(X_train, y_train, idf)
+        else:
+            model.fit(X_train, y_train)
+        predictions = model.predict(X_val)
+        results[i] = get_accuracy(predictions, y_val)
+    return results
